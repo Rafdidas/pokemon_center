fd79ac7c8784c662b0d8172a350bf179d875014c
hue <hue@wisa.co.kr>
2024-10-21 10:53:37 +0900

    build 수정 중

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   
   return (
     <Routes>
-      <Route path="/poke_dex" element={<Header/>} >
+      <Route path="/*" element={<Header/>} >
         <Route index element={<Home/>} />
         <Route path='detail/:id' element={<Detail />} />
       </Route>
